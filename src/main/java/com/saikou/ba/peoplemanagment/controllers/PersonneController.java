205f5ded8be7477d4bbb258d71891466ab722a7b
saikou <seykou534@yahoo.fr>
2022-01-05 20:27:32 +0100

    updating controller

--- src/main/java/com/saikou/ba/peoplemanagment/controllers/PersonneController.java
+++ src/main/java/com/saikou/ba/peoplemanagment/controllers/PersonneController.java
@@ -3,9 +3,13 @@ package com.saikou.ba.peoplemanagment.controllers;
 import java.util.Date;
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.saikou.ba.peoplemanagment.entities.Personne;
 import com.saikou.ba.peoplemanagment.services.PersonneService;
@@ -18,12 +22,12 @@ public class PersonneController {
 	
 	
 	@GetMapping("/allPeople")
-	public List<Personne> getAllPersonne() {
-		return personneService.getAllPeronne();
+	public ResponseEntity<List<Personne>> getAllPersonne() {
+		return  new ResponseEntity<>( personneService.getAllPeronne(), HttpStatus.OK);
 	}
 	
-	
+	@PostMapping
 	public void addPersonne(String firstName,String lastName, Date birthDate) {
-		
+		// todo: 
 	}
 }
